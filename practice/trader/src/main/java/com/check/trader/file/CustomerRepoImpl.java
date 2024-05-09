404a0c6e2fb82cf496a3180acaed8af3cc7e5b21
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2024-05-09 08:01:54 -0400

    starting repo

--- practice/trader/src/main/java/com/check/trader/file/CustomerRepoImpl.java
+++ practice/trader/src/main/java/com/check/trader/file/CustomerRepoImpl.java
@@ -6,11 +6,16 @@ import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
 import java.util.List;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 @Component
-public class CustomerRepoImpl implements CustomerRepo{
+public class CustomerRepoImpl implements CustomerRepo {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -22,7 +27,21 @@ public class CustomerRepoImpl implements CustomerRepo{
 
     @Override
     public List<Customers> fetchCustomers() {
-        logger.info("Start fetch customers from {}",fileName);
+        logger.info("Start fetch customers from {}", fileName);
+
+        var path = Paths.get(fileName);
+
+        String data = "";
+
+        try {
+            data = Files.readString(path);
+        } catch (IOException e) {
+            logger.error("Error reading data due to {}", data);
+        }
+
+        var customersString = Arrays.asList(data.split("\\R"));
+
+        logger.info("Read data: {} with size {}", data, customersString.size());
 
         return List.of();
     }
