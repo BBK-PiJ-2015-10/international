fa118f40c00e4441f4186f3b6d230f6e8269b930
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2024-06-03 09:21:08 -0400

    impl p1

--- practice/src/main/java/codility/zal/MatrixOptimizerImpl.java
+++ practice/src/main/java/codility/zal/MatrixOptimizerImpl.java
@@ -8,25 +8,25 @@ public class MatrixOptimizerImpl implements MatrixPathOptimizer {
 
     class Node {
 
-        int rowPosition;
-        int columnPosition;
+        int xPosition;
+        int yPosition;
         int number;
         List<Node> pathToNode = new LinkedList<>();
         int totalNumber;
 
-        public Node(int rowPosition, int columnPosition, int number) {
-            this.rowPosition = rowPosition;
-            this.columnPosition = columnPosition;
+        public Node(int xPosition, int yPosition, int number) {
+            this.xPosition = xPosition;
+            this.yPosition = yPosition;
             this.number = number;
             this.totalNumber = number;
         }
 
-        public int getRowPosition() {
-            return rowPosition;
+        public int getxPosition() {
+            return xPosition;
         }
 
-        public int getColumnPosition() {
-            return columnPosition;
+        public int getyPosition() {
+            return yPosition;
         }
 
         public int getNumber() {
@@ -56,21 +56,41 @@ public class MatrixOptimizerImpl implements MatrixPathOptimizer {
         var length = matrix.length;
         var height = matrix[0].length;
 
-        var maxRow = length - 1;
-        var maxColumn = 0;
+        var maxXPos = length - 1;
+        var minYPos = 0;
 
 
         Queue<Node> nodeTracker = new LinkedList<>();
         var originNode = new Node(0, height - 1, matrix[0][height - 1]);
         nodeTracker.add(originNode);
         var maxValue = originNode.totalNumber;
+        var maxNode = originNode;
 
         while (!nodeTracker.isEmpty()) {
             var node = nodeTracker.poll();
-
+            var downYPos = node.yPosition - 1;
+            var rightXPos = node.xPosition + 1;
+            if (rightXPos <= maxXPos) {
+                var rightNode = new Node(rightXPos, node.yPosition, matrix[rightXPos][node.yPosition]);
+                rightNode.setTotalNumber(rightNode.totalNumber + node.totalNumber);
+                if (rightNode.getTotalNumber() >= maxValue) {
+                    maxValue = rightNode.getTotalNumber();
+                    rightNode.pathToNode.add(node);
+                    nodeTracker.add(rightNode);
+                    maxNode = rightNode;
+                }
+            }
+            if (downYPos >= minYPos) {
+                var downNode = new Node(node.xPosition, downYPos, matrix[node.xPosition][downYPos]);
+                downNode.setTotalNumber(downNode.totalNumber + node.totalNumber);
+                if (downNode.getTotalNumber() >= maxValue) {
+                    maxValue = downNode.getTotalNumber();
+                    downNode.pathToNode.add(node);
+                    nodeTracker.add(downNode);
+                    maxNode = downNode;
+                }
+            }
         }
-        
-
-        return "";
+        return String.valueOf(maxValue);
     }
 }
