26b915139252eafa25fa963cf5c3b300764ca06d
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-01-18 15:01:10 -0500

    improve readability

--- practice/src/main/java/treesgraphs/minimumpath/dijstra/Graph.java
+++ practice/src/main/java/treesgraphs/minimumpath/dijstra/Graph.java
@@ -21,24 +21,24 @@ public class Graph {
         nodes.forEach(n -> distancesFromSource.put(n, Integer.MAX_VALUE));
 
         // create settle and unsettle sets
-        Set<Node> settledNodes = new HashSet<>();
-        Set<Node> unsettledNodes = new HashSet<>();
+        Set<Node> visitedNodes = new HashSet<>();
+        Set<Node> unvisitedNodes = new HashSet<>();
 
         distancesFromSource.put(source, 0);
-        unsettledNodes.add(source);
+        unvisitedNodes.add(source);
 
-        while (!unsettledNodes.isEmpty()) {
-            Node curretNode = getShortestDistanceNode(unsettledNodes, distancesFromSource);
-            unsettledNodes.remove(curretNode);
+        while (!unvisitedNodes.isEmpty()) {
+            Node curretNode = getShortestDistanceNode(unvisitedNodes, distancesFromSource);
+            unvisitedNodes.remove(curretNode);
             for (Map.Entry<Node, Integer> adjacencyPair : curretNode.getAdjacentNodes().entrySet()) {
                 Node adjacentNode = adjacencyPair.getKey();
                 Integer edgeWeight = adjacencyPair.getValue();
-                if (!settledNodes.contains(adjacentNode)) {
+                if (!visitedNodes.contains(adjacentNode)) {
                     calculateMinimumDistance(adjacentNode, edgeWeight, curretNode, distancesFromSource);
-                    unsettledNodes.add(adjacentNode);
+                    unvisitedNodes.add(adjacentNode);
                 }
             }
-            settledNodes.add(curretNode);
+            visitedNodes.add(curretNode);
             if (curretNode == destination) {
                 shortestDistance = distancesFromSource.get(curretNode);
                 break;
@@ -72,14 +72,20 @@ public class Graph {
 //        }
 //    }
 
-    private Node getShortestDistanceNode(Set<Node> unsettledNodes, HashMap<Node, Integer> distances) {
-        return unsettledNodes.stream().filter(n -> distances.get(n) < Integer.MAX_VALUE).min(Comparator.comparing(n -> distances.get(n))).orElseGet(null);
+    private Node getShortestDistanceNode(Set<Node> unvisitedNodes, HashMap<Node, Integer> distancesFromSource) {
+        return unvisitedNodes.stream()
+                // want to filter to only unvisited nodes that are reachable from the source or aware at that point
+                .filter(n -> distancesFromSource.get(n) < Integer.MAX_VALUE)
+                // get the one closest to the source
+                .min(Comparator.comparing(n -> distancesFromSource.get(n))).orElseGet(null);
     }
 
-    private void calculateMinimumDistance(Node evaluationNode, Integer edgeWeight, Node fromNode, HashMap<Node, Integer> distances) {
-        int fromDistance = distances.get(fromNode);
-        if (fromDistance + edgeWeight < distances.get(evaluationNode)) {
-            distances.put(evaluationNode, fromDistance + edgeWeight);
+    private void calculateMinimumDistance(Node evaluationNode, Integer edgeWeight, Node currentNode, HashMap<Node, Integer> distancesFromSource) {
+        // getting distances fromSource to the current node
+        int fromDistance = distancesFromSource.get(currentNode);
+        // comparing getting adjacent node from node vs what is on the distancesFromSource
+        if (fromDistance + edgeWeight < distancesFromSource.get(evaluationNode)) {
+            distancesFromSource.put(evaluationNode, fromDistance + edgeWeight);
             //List<Node> shortestPath = new LinkedList<>(sourceNode.getShortestPath());
             //shortestPath.add(sourceNode);
             //evaluationNode.setShortestPath(shortestPath);
