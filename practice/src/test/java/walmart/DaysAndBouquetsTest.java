14c36531e49bf8514ba59dc1be62fee27fe1e670
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-01-26 15:34:49 -0500

    flower solution

--- practice/src/test/java/walmart/DaysAndBouquetsTest.java
+++ practice/src/test/java/walmart/DaysAndBouquetsTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 //https://medium.com/@techiecontent/walmart-interview-experience-sde-iii-6f05a1fda937
 public class DaysAndBouquetsTest {
 
-    private DaysAndBouquets daysAndBouquets = new DaysAndBouquets();
+    private DaysAndBouquetsSolution daysAndBouquets = new DaysAndBouquetsSolution();
 
     @Test
     public void test1() {
@@ -60,6 +60,21 @@ public class DaysAndBouquetsTest {
         var bloomDays = new int[]{1,10,2,9,3,8,4,7,5,6};
         var numBouquetsRequired = 4;
         var numFlowersPerBouquetRequired = 2;
+
+
+        var result = daysAndBouquets.minDays(bloomDays, numBouquetsRequired, numFlowersPerBouquetRequired);
+
+
+        Assert.assertEquals(9, result);
+
+    }
+
+    @Test
+    public void test5() {
+
+        var bloomDays = new int[]{11,9,7,5,3,1};
+        var numBouquetsRequired = 2;
+        var numFlowersPerBouquetRequired = 3;
 
 
         var result = daysAndBouquets.minDays(bloomDays, numBouquetsRequired, numFlowersPerBouquetRequired);
