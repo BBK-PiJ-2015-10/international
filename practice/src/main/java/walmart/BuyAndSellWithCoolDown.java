fd4b099b37473688261fca824000fe8ac4091080
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-01-28 07:08:45 -0500

    updating wip solution

--- practice/src/main/java/walmart/BuyAndSellWithCoolDown.java
+++ practice/src/main/java/walmart/BuyAndSellWithCoolDown.java
@@ -26,29 +26,40 @@ public class BuyAndSellWithCoolDown {
         }
     }
 
-    private int add(int price, Node node) {
-        var profit = 0;
-        // evaluate left
-
-
-        // evaluate right
-        return profit;
+    private int add(int price, int period, Node node) {
+        var leftProfit = 0;
+        var rightProfit = 0;
+        if (node.left != null || node.right != null) {
+            leftProfit = add(price, period, node.left);
+            rightProfit = add(price, period, node.right);
+        } else {
+            leftProfit = takeAction(price, period, node);
+            rightProfit = leftProfit;
+        }
+        if (leftProfit >= rightProfit) {
+            return leftProfit;
+        } else {
+            return rightProfit;
+        }
     }
 
     private int takeAction(int price, int period, Node node) {
-        var profit = 0;
-        Node decision = null;
-        var existingProfit = node.profit;
-        var existingCost = node.cost;
+        var cumulativeProfit = node.profit;
         switch (node.state) {
             case BUY:
-                System.out.println("WOOF");
-                //var incProfit = p
-                // you can sell
-                //decision = new Node(State.SELL,
-                //      )
-
-                // you can hold
+                if (price > node.cost) {
+                    // left do nothing, right sell
+                    var incProfit = price - node.cost;
+                    var newCost = price;
+                    var updatedCumProfit = node.profit + incProfit;
+                    var leftNode = new Node(node.state,node.period,node.profit,node.cost,null,null);
+                    var rightNode = new Node(State.SELL,period,updatedCumProfit,newCost,null,null);
+                    node.left = leftNode;
+                    node.right = rightNode;
+                    cumulativeProfit = updatedCumProfit;
+                } else {
+                    // do nothing don't add any nodes
+                }
                 break;
             case SELL:
                 System.out.println("WOOF");
@@ -61,7 +72,7 @@ public class BuyAndSellWithCoolDown {
                 // you can nothing
                 break;
         }
-        return profit;
+        return cumulativeProfit;
     }
 
     public int maxProfit(int[] prices) {
@@ -70,8 +81,9 @@ public class BuyAndSellWithCoolDown {
             return maxProfit;
         }
         var rootNode = new Node(State.NOTHING, 0, 0, 0, null, null);
-        for (int price : prices) {
-            var profit = add(price, rootNode);
+        for (int i = 0; i < prices.length; i++) {
+            var price = prices[i];
+            var profit = add(price, i, rootNode);
             if (profit > maxProfit) {
                 maxProfit = profit;
             }
