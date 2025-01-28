3635e4799dcd2820ae9f51473cbf610c9eec636c
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-01-28 15:07:52 -0500

    adding solution p2

--- practice/src/test/java/walmart/BuyAndSellWithCoolDownTest.java
+++ practice/src/test/java/walmart/BuyAndSellWithCoolDownTest.java
@@ -16,4 +16,13 @@ public class BuyAndSellWithCoolDownTest {
         Assert.assertEquals(3, maxProfit);
 
     }
+
+    @Test
+    public void test205() {
+
+        var prices = new int[]{48,12,60,93,97,42,25,64,17,56,85,93,9,48,52,42,58,85,81,84,69,36,1,54,23,15,72,15,11,94};
+        var maxProfit = calculator.maxProfit(prices);
+        Assert.assertEquals(3, maxProfit);
+
+    }
 }
