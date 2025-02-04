958e1fd3a3b394dbf3d9f120e7e6c9cb16bb29e9
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-02-04 18:15:26 -0500

    min-heap solution

--- practice/src/test/java/palantir/KthLargest.java
+++ practice/src/test/java/palantir/KthLargest.java
@@ -2,14 +2,15 @@ package palantir;
 
 public class KthLargest {
 
-    public int kthLargest(int k, int[] array) {
-        var set = new CustomSortedTreeSet(k);
-        for (int i = 0; i < array.length; i++) {
-            var elementValue = array[i];
-            set.add(elementValue);
-        }
-        return se
-    }
+    //https://www.geeksforgeeks.org/kth-largest-element-in-an-array/
+
+//    public int kthLargest(int k, int[] array) {
+//        var set = new CustomSortedTreeSet(k);
+//        for (int i = 0; i < array.length; i++) {
+//            var elementValue = array[i];
+//            set.add(elementValue);
+//        }
+//    }
 
 
 
