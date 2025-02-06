1f15d35a5f4ce75dfc9385ddf7607f9b5adcf21a
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-02-05 19:22:51 -0500

    updating solution via arrrays

--- practice/src/main/java/palantir/KthLargestOnArray.java
+++ practice/src/main/java/palantir/KthLargestOnArray.java
@@ -1,8 +1,9 @@
 package palantir;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class KthLargestOnArray {
 
@@ -25,6 +26,40 @@ public class KthLargestOnArray {
         }
         result = priorityQueue.poll();
         return result;
+    }
+
+    public int getKthLargestViaArrays(int k, int[] array) {
+        var result = -1;
+        if (array.length < k) {
+            return result;
+        }
+        var listInput = Arrays.stream(array).boxed().collect(Collectors.toList());
+        return getKthLargestViaArraysHelper(k, listInput);
+    }
+
+    private int getKthLargestViaArraysHelper(int k, List<Integer> input) {
+        int pivotPosition = new Random().nextInt(input.size());
+        Integer pivot = input.get(pivotPosition);
+        List<Integer> lowerValuesArray = new LinkedList<>();
+        List<Integer> higherValuesArray = new LinkedList<>();
+        List<Integer> sameValueArray = new LinkedList<>();
+        for (Integer value : input) {
+            if (value > pivot) {
+                higherValuesArray.add(value);
+            } else if (value < pivot) {
+                lowerValuesArray.add(value);
+            } else {
+                sameValueArray.add(value);
+            }
+        }
+        if (higherValuesArray.size() > k) {
+            return getKthLargestViaArraysHelper(k, higherValuesArray);
+            // need to go to right
+        } else if (higherValuesArray.size() + sameValueArray.size() < k) {
+            var ktoFind = k - higherValuesArray.size() - sameValueArray.size();
+            return getKthLargestViaArraysHelper(k, lowerValuesArray);
+        }
+        return pivot;
     }
 
 
