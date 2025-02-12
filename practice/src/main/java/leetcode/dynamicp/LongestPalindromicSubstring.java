2625b5c962fbcab4350b88f83fa76deaa8716d6b
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-02-12 07:16:12 -0500

    impl sol p1

--- practice/src/main/java/leetcode/dynamicp/LongestPalindromicSubstring.java
+++ practice/src/main/java/leetcode/dynamicp/LongestPalindromicSubstring.java
@@ -8,14 +8,41 @@ public class LongestPalindromicSubstring {
     }
 
     public String longestPalindrome(String s) {
-
+        var longestSubstring = s.substring(0,1);
         for (int i = 0; i <= s.length(); i++) {
             for (int k = i + 1; k <= s.length(); k++) {
                 var subString = s.substring(i, k);
                 System.out.println(String.format("i is %s, k is %s, substring is %s", i, k, subString));
+                if (longestSubstring.length()<subString.length()){
+                    var isPalindrome = isPalindrome(subString);
+                    if (isPalindrome){
+                        longestSubstring = subString;
+                    }
+                } else {
+                    //break;
+                }
             }
         }
         return "";
+    }
+
+    private boolean isPalindrome(String input) {
+        if (input.length() == 1) {
+            return true;
+        } else {
+            var left = input.substring(0, 1);
+            var right = input.substring(input.length() - 1, input.length());
+            if (left.equals(right)) {
+                var middle = input.substring(1, input.length() - 1);
+                if (middle.isEmpty()) {
+                    return true;
+                } else {
+                    return isPalindrome(middle);
+                }
+            } else {
+                return false;
+            }
+        }
     }
 
 }
--- practice/src/test/java/leetcode/dynamicp/LongestPalindromicSubstringTest.java
+++ practice/src/test/java/leetcode/dynamicp/LongestPalindromicSubstringTest.java
@@ -7,6 +7,25 @@ public class LongestPalindromicSubstringTest {
 
     LongestPalindromicSubstring calculator = new LongestPalindromicSubstring();
 
+    @Test
+    public void test0(){
+
+        String input = "ab";
+
+        var left = input.substring(0,1);
+        var right = input.substring(input.length()-1,input.length());
+        var middle = input.substring(1,input.length()-1);
+        var middleIsEmpty = middle.isEmpty();
+
+        System.out.println(String.format("Left is %s",left));
+        System.out.println(String.format("Right is %s", right));
+        System.out.println(String.format("Middle is %s",middle));
+        System.out.println(String.format("Middle is empty %s",middleIsEmpty));
+
+        Assert.assertEquals(true,true);
+
+    }
+
 
     @Test
     public void test1(){
@@ -23,6 +42,17 @@ public class LongestPalindromicSubstringTest {
     public void test2(){
 
         String input = "abc";
+
+        var isDivisiable = calculator.longestPalindrome(input);
+
+        Assert.assertEquals(true,true);
+
+    }
+
+    @Test
+    public void test4(){
+
+        String input = "abcde";
 
         var isDivisiable = calculator.longestPalindrome(input);
 
