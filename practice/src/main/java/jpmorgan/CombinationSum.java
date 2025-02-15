7a1b35dc31bbae664e1c8d565722d304eea66c83
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-02-15 09:02:39 -0500

    draft solution

--- practice/src/main/java/jpmorgan/CombinationSum.java
+++ practice/src/main/java/jpmorgan/CombinationSum.java
@@ -2,6 +2,7 @@ package jpmorgan;
 
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -13,6 +14,13 @@ public class CombinationSum {
 
     private Logger logger = Logger.getLogger("CombinationSum");
 
+
+    private List<Integer> helperAdded(List<Integer> input, Integer left, Integer right) {
+        input.add(left);
+        input.add(right);
+        return input;
+    }
+
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> solution = new LinkedList<>();
         var sortedInput = Arrays.stream(candidates)
@@ -20,41 +28,35 @@ public class CombinationSum {
                 .filter(n -> n < target)
                 .sorted()
                 .collect(Collectors.toList());
-        for (int i=0;i<sortedInput.size();i++){
-            //var iSolutions = new LinkedList<Integer>();
-            var iVisitedPotential = new LinkedList<Integer>();
-            for (int k=sortedInput.size()-1;k>i;k--){
+        for (int i = 0; i < sortedInput.size(); i++) {
+            var iVisitedSum = new HashMap<Integer, List<List<Integer>>>();
+            for (int k = sortedInput.size() - 1; k > i; k--) {
                 var left = sortedInput.get(i);
                 var right = sortedInput.get(k);
                 var sum = left + right;
-                if (sum == target){
+                var diffToTarget = target - sum;
+                if (diffToTarget == 0) {
                     var iSolution = new LinkedList<Integer>();
                     iSolution.add(left);
                     iSolution.add(right);
                     solution.add(iSolution);
-                    iVisitedPotential.clear();
                 } else {
-                    if (sum < target){
-                        if (iVisitedPotential.isEmpty()==false){
-                            var tail = iVisitedPotential.get(iVisitedPotential.size()-1);
-                            var sumWithTail = sum + tail;
-                            if (sumWithTail==target){
-                                var iSolution = new LinkedList<Integer>();
-                                iSolution.add(left);
-                                iSolution.add(right);
-                                iSolution.add(tail);
-                                solution.add(iSolution);
-                                iVisitedPotential.clear();
-                            } else if (sumWithTail < target){
-                                iVisitedPotential.add(right);
-                            } else {
-                                var removed = iVisitedPotential.removeLast();
-                                iVisitedPotential.add(right);
+                    if (diffToTarget > 0) {
+                        var exitingSolution = iVisitedSum.get(diffToTarget);
+                        if (exitingSolution != null) {
+                            iVisitedSum.remove(diffToTarget).stream().map(as -> helperAdded(as, left, right))
+                                    .forEach(d -> solution.add(d));
+                        } else {
+                            var existingSums = iVisitedSum.keySet();
+                            for (Integer existingSum : existingSums) {
+                                if (existingSum < diffToTarget) {
+                                    iVisitedSum.get(existingSum).forEach(el -> el.add(right));
+                                }
                             }
                         }
                     }
                 }
-                logger.info(String.format("Comparing pos %d value %d ver pos %d value %d",i,left,k,right));
+                logger.info(String.format("Done Comparing pos %d value %d ver pos %d value %d", i, left, k, right));
             }
         }
         return null;
