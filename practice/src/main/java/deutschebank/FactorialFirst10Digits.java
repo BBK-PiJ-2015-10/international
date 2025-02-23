84b1cc003a4b437a759da3e6b1847c86cc17089f
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-02-23 11:16:55 -0500

    adding solution p3

---
+++ practice/src/main/java/deutschebank/FactorialFirst10Digits.java
@@ -0,0 +1,21 @@
+package deutschebank;
+
+import java.math.BigInteger;
+
+public class FactorialFirst10Digits {
+
+    public int factorial(int input) {
+        return factorialCum(input, 1);
+    }
+
+    public int factorialCum(int input, Integer accum) {
+        if (input == 1) {
+            return accum;
+        } else {
+            var newAccum = accum * input;
+            return factorialCum(input - 1, newAccum);
+        }
+    }
+
+
+}
---
+++ practice/src/test/java/deutschebank/FactorialFirst10DigitsTest.java
@@ -0,0 +1,37 @@
+package deutschebank;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FactorialFirst10DigitsTest {
+
+    private FactorialFirst10Digits calculator = new FactorialFirst10Digits();
+
+    @Test
+    public void test1() {
+
+        var input = 2;
+        var factorial = calculator.factorial(input);
+        Assert.assertEquals(2, factorial);
+
+    }
+
+    @Test
+    public void test2() {
+
+        var input = 3;
+        var factorial = calculator.factorial(input);
+        Assert.assertEquals(6, factorial);
+
+    }
+
+    @Test
+    public void test3() {
+
+        var input = 10;
+        var factorial = calculator.factorial(input);
+        Assert.assertEquals(3628800, factorial);
+
+    }
+}
+
