51f3420b94ab1004b8244b5c2365b25d9476f234
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-02-27 10:55:22 -0500

    adding optimal solution

--- practice/src/main/java/inter/patterns/prefixsums/KsumArrays.java
+++ practice/src/main/java/inter/patterns/prefixsums/KsumArrays.java
@@ -1,5 +1,8 @@
 package inter.patterns.prefixsums;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class KsumArrays {
 
     public int findNumberOfSubArrays(int[] array, int target) {
@@ -28,5 +31,22 @@ public class KsumArrays {
             }
         }
         return result;
+    }
+
+    public int findNumberOfSubArraysOptimized(int[] array, int target) {
+        Map<Integer, Integer> prefixSums = new HashMap<>();
+        prefixSums.put(0, 1);
+        int counter = 0;
+        int currentCum = 0;
+        for (int i = 0; i < array.length; i++) {
+            currentCum = currentCum + array[i];
+            int expectedPriorCum = currentCum - target;
+            var existingPriorCumCount = prefixSums.get(expectedPriorCum);
+            if (existingPriorCumCount != null) {
+                counter ++;
+            }
+            prefixSums.put(currentCum,1);
+        }
+        return counter;
     }
 }
--- practice/src/test/java/intern/patterns/prefixsums/KsumArraysTest.java
+++ practice/src/test/java/intern/patterns/prefixsums/KsumArraysTest.java
@@ -4,7 +4,6 @@ import inter.patterns.prefixsums.KsumArrays;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.List;
 
 public class KsumArraysTest {
 
@@ -31,6 +30,30 @@ public class KsumArraysTest {
         var result = calculator.findNumberOfSubArrays(input, target);
 
         Assert.assertEquals(2, result);
+
+    }
+
+    @Test
+    public void test3() {
+
+        int[] input = {1, 1, 1};
+        int target = 2;
+
+        var result = calculator.findNumberOfSubArraysOptimized(input, target);
+
+        Assert.assertEquals(2, result);
+
+    }
+
+    @Test
+    public void test4() {
+
+        int[] input = {1, 2, -1, 1, 2};
+        int target = 3;
+
+        var result = calculator.findNumberOfSubArraysOptimized(input, target);
+
+        Assert.assertEquals(3, result);
 
     }
 }
