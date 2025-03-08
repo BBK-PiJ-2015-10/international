ad98ec448b4a2c973788059275939727de32862c
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-03-08 16:00:26 -0500

    base implementation find first and last occurrence

--- practice/src/test/java/intern/patterns/binarysearch/FindFirstAndLastOccurrencesTest.java
+++ practice/src/test/java/intern/patterns/binarysearch/FindFirstAndLastOccurrencesTest.java
@@ -24,4 +24,68 @@ public class FindFirstAndLastOccurrencesTest {
         Assert.assertEquals(expectedResult, Arrays.stream(result).boxed().toList());
 
     }
+
+    @Test
+    public void test2() {
+
+        int target = 5;
+        int[] array = {1, 2, 3, 4, 4, 4, 4, 6, 7, 8, 9, 10, 11};
+
+        var result = calculator.findFirstAndLastOccurrences(array, target);
+
+        int[] expected = {-1, -1};
+        var expectedResult = Arrays.stream(expected).boxed().toList();
+
+        Assert.assertEquals(expectedResult, Arrays.stream(result).boxed().toList());
+
+    }
+
+    @Test
+    public void test3() {
+
+        int target = 5;
+        int[] array = {1, 2, 3, 4, 4, 4, 5, 6, 7, 8, 9, 10, 11};
+
+        var result = calculator.findFirstAndLastOccurrences(array, target);
+
+        int[] expected = {6, 6};
+        var expectedResult = Arrays.stream(expected).boxed().toList();
+
+        Assert.assertEquals(expectedResult, Arrays.stream(result).boxed().toList());
+
+    }
+
+    @Test
+    public void test4() {
+
+        int target = 1;
+        int[] array = {1, 2, 3, 4, 4, 4, 5, 6, 7, 8, 9, 10, 11};
+
+        var result = calculator.findFirstAndLastOccurrences(array, target);
+
+        int[] expected = {0, 0};
+        var expectedResult = Arrays.stream(expected).boxed().toList();
+
+        Assert.assertEquals(expectedResult, Arrays.stream(result).boxed().toList());
+
+    }
+
+    @Test
+    public void test5() {
+
+        int target = 3;
+        int[] array = {1, 2, 3};
+
+        var result = calculator.findFirstAndLastOccurrences(array, target);
+
+        int[] expected = {2,2};
+        var expectedResult = Arrays.stream(expected).boxed().toList();
+
+        Assert.assertEquals(expectedResult, Arrays.stream(result).boxed().toList());
+
+    }
+
+
+
+
 }
