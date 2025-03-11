4c34a803f5e55bd0d59eefc194a1937ad84ab4c8
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-03-11 07:17:40 -0400

    starting solution

--- practice/src/test/java/intern/patterns/intervals/MergingOverlappingIntervalsTest.java
+++ practice/src/test/java/intern/patterns/intervals/MergingOverlappingIntervalsTest.java
@@ -1,21 +1,29 @@
 package intern.patterns.intervals;
 
+import inter.patterns.intervals.MergingOverlappingIntervals;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MergingOverlappingIntervalsTest {
 
+    private MergingOverlappingIntervals calculator = new MergingOverlappingIntervals();
+
     @Test
     public void test1(){
 
-        int[][] cat = new int[5][2];
-        cat[0][0] = 3;
-        cat[4][1] = 4;
+        int[][] intervals = new int[5][2];
+        intervals[0][0] = 3;
+        intervals[0][1] = 4;
+        intervals[1][0] = 7;
+        intervals[1][1] = 8;
+        intervals[2][0] = 2;
+        intervals[2][1] = 5;
+        intervals[3][0] = 6;
+        intervals[3][1] = 7;
+        intervals[4][0] = 1;
+        intervals[4][1] = 4;
 
-        var dog = cat[0];
-
-        System.out.println(cat.length);
-        System.out.println(dog.length);
+        var result = calculator.findOverlappingIntervals(intervals);
 
 
         Assert.assertEquals(true,true);
