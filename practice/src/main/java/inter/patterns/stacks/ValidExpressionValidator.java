d3977c9705d1247d02416e60c927aeda3def5977
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-03-16 11:15:26 -0400

    stack solution

--- practice/src/main/java/inter/patterns/stacks/ValidExpressionValidator.java
+++ practice/src/main/java/inter/patterns/stacks/ValidExpressionValidator.java
@@ -1,8 +1,8 @@
 package inter.patterns.stacks;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
 import java.util.logging.Logger;
 
 public class ValidExpressionValidator {
@@ -11,41 +11,29 @@ public class ValidExpressionValidator {
 
     private Set<Character> openParenthesis = Set.of('(', '{', '[');
     private Map<Character, Character> closeToOpen = Map.of(')', '(', '}', '{', ']', '[');
-    
+
     public boolean isValidExpression(String input) {
-        Map<Character, Integer> existingOpenParenthesis = new HashMap<>();
-        Boolean result = false;
         if (input.isEmpty() || input.length() % 2 != 0) {
             return false;
         }
-        var inputCharacters = input.toCharArray();
-        for (int i = 0; i < inputCharacters.length; i++) {
-            Character parenthesis = inputCharacters[i];
-            var isOpenParenthesis = openParenthesis.contains(parenthesis);
-            if (isOpenParenthesis) {
-                var existingOpenCount = existingOpenParenthesis.get(parenthesis);
-                logger.info(String.format("%s is open parenthesis", parenthesis));
-                if (existingOpenCount == null) {
-                    logger.info(String.format("Updating %s with count %s", parenthesis, 1));
-                    existingOpenParenthesis.put(parenthesis, 1);
-                } else {
-                    existingOpenParenthesis.put(parenthesis, existingOpenCount + 1);
-                }
+        Stack<Character> openStack = new Stack<>();
+        var characters = input.toCharArray();
+        for (int i = 0; i < characters.length; i++) {
+            Character parenthesis = characters[i];
+            if (openParenthesis.contains(parenthesis)) {
+                openStack.push(parenthesis);
             } else {
-                logger.info(String.format("%s is closed parenthesis", parenthesis));
-                var openParenthesis = closeToOpen.get(parenthesis);
-                var existingOpenCount = existingOpenParenthesis.get(openParenthesis);
-                if (existingOpenCount == null) {
+                var expectedOpen = closeToOpen.get(parenthesis);
+                if (openStack.isEmpty()) {
                     return false;
                 }
-                if (existingOpenCount <= 1) {
-                    existingOpenParenthesis.remove(openParenthesis);
-                } else {
-                    existingOpenParenthesis.put(openParenthesis, existingOpenCount - 1);
+                var latestOpen = openStack.pop();
+                if (latestOpen == null || latestOpen.equals(expectedOpen) == false) {
+                    return false;
                 }
             }
         }
-        return existingOpenParenthesis.isEmpty();
+        return openStack.isEmpty();
     }
 
 }
