ea813fe139b8a398a1b4405f4c3cdda3d772f085
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-03-31 18:31:04 -0400

    longest repeatig with overlap

---
+++ practice/src/main/java/veinteream/FindMaxRepeatingSubstring.java
@@ -0,0 +1,22 @@
+package veinteream;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+
+public class Question4 {
+
+    // longest recurring substring
+    // "abcabca" => abca
+    // there will always be only one anaswe
+    public String findSubstring(String text) {
+        int maxSize = 1 + (text.length() / 2);
+        Map<MysqlxDatatypes.Scalar.String,Integer>
+        while (maxSize > 0){
+
+
+        }
+
+        return "";
+    }
+
+
+}
---
+++ practice/src/test/java/veinteream/FindMaxRepeatingSubstringTest.java
@@ -0,0 +1,4 @@
+package veinteream;
+
+public class FindMaxRepeatingSubstringTest {
+}
