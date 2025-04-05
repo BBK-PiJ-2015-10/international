319668fcf6e520de08987e584b40d51ddc3b501f
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-04-05 10:49:45 -0400

    adding solution to word pattern matcher

--- practice/src/main/java/veinteream/WordsFiltering.java
+++ practice/src/main/java/veinteream/WordsFiltering.java
@@ -1,21 +1,35 @@
 package veinteream;
 
-import java.util.ArrayList;
+
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WordsFiltering {
 
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
     public List<String> filterWords(String[] words, String letters) {
         List<String> filteredWords = new LinkedList<>();
-
+        String regex = "";
+        for (int i = 0; i < letters.length(); i++) {
+            if (i == 0) {
+                regex = regex + letters.substring(i, i + 1);
+            } else {
+                regex = regex + "|" + letters.substring(i, i + 1);
+            }
+        }
+        Pattern pattern = Pattern.compile(regex);
         for (int i = 0; i < words.length; i++) {
             var word = words[i];
-            if (word.matches("[ae]")) {
+            Matcher matcher = pattern.matcher(word);
+            if (matcher.find()) {
+                logger.info(String.format("Adding word %s", word));
                 filteredWords.add(word);
             }
         }
-        //String[] result = (String[]) filteredWords.toArray();
         return filteredWords;
     }
 
