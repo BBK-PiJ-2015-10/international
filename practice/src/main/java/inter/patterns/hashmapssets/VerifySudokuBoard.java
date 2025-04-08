24a97f0ae3c9adb32c5f88ed8c0fd6d32a188738
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-04-08 06:25:18 -0400

    base solution of sudoko

--- practice/src/main/java/inter/patterns/hashmapssets/VerifySudokuBoard.java
+++ practice/src/main/java/inter/patterns/hashmapssets/VerifySudokuBoard.java
@@ -1,9 +1,6 @@
 package inter.patterns.hashmapssets;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class VerifySudokuBoard {
@@ -11,13 +8,68 @@ public class VerifySudokuBoard {
     private Logger logger = Logger.getLogger(this.getClass().getName());
 
     private int solveQuandrant(Integer position) {
-        if (position < 3) {
-            return 1;
+        return position / 3;
+    }
+
+    public boolean isValidSudokoBoard(char[][] board) {
+        // length = 9
+        Map<Integer, HashSet<Integer>> rows = new HashMap<>();
+        Map<Integer, HashSet<Integer>> columns = new HashMap<>();
+        Map<Integer, HashSet<Integer>> grid = new HashMap<>();
+        int xbase = 0;
+        int ybase = 0;
+        while (xbase < 7 && ybase < 7) {
+            for (int x = xbase; x <= xbase + 2; x++) {
+                for (int y = ybase; y <= ybase + 3; y++) {
+                    Character charValue = board[x][y];
+                    if (Character.isDigit(charValue)) {
+                        var value = Integer.valueOf(charValue.toString());
+                        logger.info(String.format("Evaluating x %d, y %d with value %d", x, y, value));
+                        var existingOnRow = rows.get(x);
+                        if (existingOnRow == null) {
+                            HashSet<Integer> newExistingRow = new HashSet<>();
+                            newExistingRow.add(value);
+                            rows.put(x, newExistingRow);
+                        } else {
+                            if (existingOnRow.contains(value)) {
+                                logger.info(String.format("Invalid Sudoko row at x %d y %d with value %d", x, y, value));
+                                return false;
+                            } else {
+                                existingOnRow.add(value);
+                            }
+                        }
+                        var existingOnColumn = columns.get(y);
+                        if (existingOnColumn == null) {
+                            HashSet<Integer> newExistingColumn = new HashSet<>();
+                            newExistingColumn.add(value);
+                            rows.put(y, newExistingColumn);
+                        } else {
+                            if (existingOnColumn.contains(value)) {
+                                logger.info(String.format("Invalid Sudoko column at x %d y %d with value %d", x, y, value));
+                                return false;
+                            } else {
+                                existingOnColumn.add(value);
+                            }
+                        }
+                        var quadrant = solveQuandrant(x);
+                        var existingQuadrant = grid.get(quadrant);
+                        if (existingQuadrant == null) {
+                            HashSet<Integer> newExistingQuadrant = new HashSet<>();
+                            existingQuadrant.add(value);
+                            rows.put(quadrant, newExistingQuadrant);
+                        } else {
+                            if (existingQuadrant.contains(value)) {
+                                logger.info(String.format("Invalid Quadrant at quadrant with value %d", quadrant, value));
+                                return false;
+                            } else {
+                                existingQuadrant.add(value);
+                            }
+                        }
+                    }
+                }
+            }
         }
-        if (position < 6) {
-            return 2;
-        }
-        return 3;
+        return true;
     }
 
     public boolean isValidSudokoBoard(int[][] board) {
