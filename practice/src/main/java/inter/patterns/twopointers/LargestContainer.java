0d9a4fdda0d96a196a0a4fe8337078ff56b7dd99
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-04-09 10:43:41 -0400

    naive solution for containers

--- practice/src/main/java/inter/patterns/twopointers/LargestContainer.java
+++ practice/src/main/java/inter/patterns/twopointers/LargestContainer.java
@@ -2,7 +2,7 @@ package inter.patterns.twopointers;
 
 public class LargestContainer {
 
-    public int largestContainer(int[] bars) {
+    public int largestContainerBrutal(int[] bars) {
         int largest = 0;
         for (int i = 0; i < bars.length - 1; i++) {
             for (int k = i + 1; k < bars.length; k++) {
@@ -15,6 +15,32 @@ public class LargestContainer {
             }
         }
         return largest;
+    }
+
+    public int largestContainer(int[] bars) {
+        int maxVolume = 0;
+        int l = 0;
+        int r = bars.length - 1;
+        while (l < r) {
+            int width = r - l;
+            int leftHeight = bars[l];
+            int rightHeight = bars[r];
+            int height = Math.min(leftHeight, rightHeight);
+            int volume = width * height;
+            if (volume > maxVolume) {
+                maxVolume = volume;
+            }
+            if (leftHeight == rightHeight) {
+                ++l;
+                --r;
+            }
+            if (leftHeight < rightHeight) {
+                ++l;
+            } else {
+                --r;
+            }
+        }
+        return maxVolume;
     }
 
 }
--- practice/src/test/java/intern/patterns/twopointers/LargestContainerTest.java
+++ practice/src/test/java/intern/patterns/twopointers/LargestContainerTest.java
@@ -9,15 +9,81 @@ public class LargestContainerTest {
     private LargestContainer container = new LargestContainer();
 
     @Test
-    public void test1(){
+    public void test1() {
 
-        int[] numbers = {2,7,8,3,7,6};
+        int[] heights = {2, 7, 8, 3, 7, 6};
 
-        var result = container.largestContainer(numbers);
+        var result = container.largestContainer(heights);
 
-        Assert.assertEquals(24,result);
-
+        Assert.assertEquals(24, result);
 
     }
+
+    @Test
+    public void test2() {
+
+        int[] heights = {};
+
+        var result = container.largestContainer(heights);
+
+        Assert.assertEquals(0, result);
+
+    }
+
+    @Test
+    public void test3() {
+
+        int[] heights = {1};
+
+        var result = container.largestContainer(heights);
+
+        Assert.assertEquals(0, result);
+
+    }
+
+    @Test
+    public void test4() {
+
+        int[] heights = {0, 1, 0};
+
+        var result = container.largestContainer(heights);
+
+        Assert.assertEquals(0, result);
+
+    }
+
+    @Test
+    public void test5() {
+
+        int[] heights = {3, 3, 3, 3};
+
+        var result = container.largestContainer(heights);
+
+        Assert.assertEquals(9, result);
+
+    }
+
+    @Test
+    public void test6() {
+
+        int[] heights = {1, 2, 3};
+
+        var result = container.largestContainer(heights);
+
+        Assert.assertEquals(2, result);
+
+    }
+
+    @Test
+    public void test7() {
+
+        int[] heights = {3, 2, 1};
+
+        var result = container.largestContainer(heights);
+
+        Assert.assertEquals(2, result);
+
+    }
+
 
 }
