d3c0578fa75a69d8be1e19690eb205af643d4893
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-05-06 17:21:49 -0400

    removing kth node

---
+++ practice/src/main/java/inter/patterns/linkedlist/RemoveKthLastNode.java
@@ -0,0 +1,38 @@
+package inter.patterns.linkedlist;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class RemoveKthLastNode {
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    public Node removedKthLastNode(Node root, int kth) {
+        int position = 0;
+        Node existing = root;
+        Map<Integer, Node> positionNode = new HashMap<>();
+        while (existing != null) {
+            positionNode.put(position, existing);
+            position++;
+            existing = existing.next;
+        }
+        int priorToKthPosition = position - kth - 1;
+        int kthPostion = position - kth;
+        int nextToKthPosition = position - kth +1;
+        logger.info(String.format("Size is %d, priorToKth pos is %d, nextToKth is %d ",position,priorToKthPosition,nextToKthPosition));
+        var priorToKth = positionNode.get(priorToKthPosition);
+        if (priorToKth != null) {
+            var nextToKth = positionNode.get(nextToKthPosition);
+            var kthnode = positionNode.get(kthPostion);
+            kthnode.next = null;
+            priorToKth.next = nextToKth;
+            logger.info(String.format("Prior to kth value is %d, next to kth value is %d",priorToKth.value,nextToKth.value));
+            return root;
+        } else {
+            logger.info("HERE2");
+            return null;
+        }
+    }
+
+}
---
+++ practice/src/test/java/intern/patterns/linkedlist/RemoveKthLastNodeTest.java
@@ -0,0 +1,37 @@
+package intern.patterns.linkedlist;
+
+import inter.patterns.linkedlist.Node;
+import inter.patterns.linkedlist.RemoveKthLastNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class RemoveKthLastNodeTest {
+
+    private RemoveKthLastNode calculator = new RemoveKthLastNode();
+
+    @Test
+    public void test1() {
+
+        Node three = new Node(3);
+        Node seven = new Node(7, three);
+        Node four = new Node(4, seven);
+        Node two = new Node(2, four);
+        Node one = new Node(1, two);
+
+        Node result = calculator.removedKthLastNode(one, 2);
+
+        List<Integer> resultFormated = new LinkedList<>();
+        while (result != null) {
+            System.out.println(String.format("Node is %s with next %s", result, result.next));
+            resultFormated.add(result.value);
+            result = result.next;
+        }
+
+        Assert.assertEquals(resultFormated, List.of(1, 2, 4, 3));
+
+    }
+
+}
