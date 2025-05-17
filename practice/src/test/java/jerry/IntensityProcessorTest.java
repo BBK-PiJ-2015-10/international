4aeda17b8c3f7c26e2e6d876b9947dc440e71c1d
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-05-17 07:40:09 -0400

    adding basic test p3

--- practice/src/test/java/jerry/IntensityProcessorTest.java
+++ practice/src/test/java/jerry/IntensityProcessorTest.java
@@ -12,6 +12,7 @@ public class IntensityProcessorTest {
     public void test1() {
 
         IntensityProcessor processor = new IntensityProcessor();
+
         var result = processor.add(10, 30, 1);
         var expectedResult = List.of(new Segment(10, 1), new Segment(30, 0));
 
@@ -28,7 +29,20 @@ public class IntensityProcessorTest {
         var result = processor.add(20, 40, 1);
 
         var expectedResult = List.of(new Segment(10, 1), new Segment(20, 2), new Segment(30, 1), new Segment(40, 0));
+        Assert.assertEquals(expectedResult, result);
 
+    }
+
+    @Test
+    public void test3() {
+
+        IntensityProcessor processor = new IntensityProcessor();
+
+        processor.add(10, 30, 1);
+        processor.add(20, 40, 1);
+        var result = processor.add(10, 40, -2);
+
+        var expectedResult = List.of(new Segment(10, -1), new Segment(20, 0), new Segment(30, -1), new Segment(40, 0));
         Assert.assertEquals(expectedResult, result);
 
     }
