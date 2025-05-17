04b0fc43bbdf4877932ec10c4c7e26cee9f61de9
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-05-17 09:15:12 -0400

    adding basic test p5

--- practice/src/main/java/jerry/IntensityProcessor.java
+++ practice/src/main/java/jerry/IntensityProcessor.java
@@ -1,7 +1,7 @@
 package jerry;
 
 import java.util.*;
-import java.util.logging.Level;
+import java.util.function.BinaryOperator;
 import java.util.logging.Logger;
 
 public class IntensityProcessor {
@@ -10,7 +10,75 @@ public class IntensityProcessor {
 
     private SortedMap<Integer, Integer> segmentIntensityRing = new TreeMap<>();
 
+    private List<Segment> segmentBinaryOperation(int fromSegment, int toSegment, int intensity, BinaryOperator<Integer> operation) {
+        if (segmentIntensityRing.isEmpty()) {
+            segmentIntensityRing.put(fromSegment, intensity);
+            segmentIntensityRing.put(toSegment, 0);
+        } else {
+            var rangeSegments = segmentIntensityRing.subMap(fromSegment, toSegment);
+            for (var segment : rangeSegments.keySet()) {
+                var existingIntensity = rangeSegments.get(segment);
+                var updatedIntensity = operation.apply(existingIntensity, intensity);
+                rangeSegments.put(segment, updatedIntensity);
+            }
+            // check if
+            var existingFromIntensity = rangeSegments.get(fromSegment);
+            if (existingFromIntensity == null) {
+                var priorSegments = segmentIntensityRing.headMap(fromSegment);
+                if (priorSegments.isEmpty()) {
+                    segmentIntensityRing.put(fromSegment, intensity);
+                } else {
+                    var beforeSegment = priorSegments.lastKey();
+                    var beforeSegmentIntensity = priorSegments.get(beforeSegment);
+                    var fromSegmentIntensity = operation.apply(beforeSegmentIntensity, intensity);
+                    segmentIntensityRing.put(fromSegment, fromSegmentIntensity);
+                }
+            }
+            // remove fromSegment if intensity is 0
+            var updatedFromSegmentIntensity = segmentIntensityRing.get(fromSegment);
+            if (updatedFromSegmentIntensity == 0) {
+                segmentIntensityRing.remove(fromSegment);
+            }
+            var nextSegments = segmentIntensityRing.tailMap(toSegment);
+            if (nextSegments.isEmpty()) {
+                segmentIntensityRing.put(toSegment, 0);
+            } else {
+                var firstNextSegment = nextSegments.firstKey();
+                if (firstNextSegment > fromSegment) {
+                    segmentIntensityRing.put(toSegment, nextSegments.get(firstNextSegment));
+                }
+            }
+            // remove toSegment if prior intensity is 0
+            var priorToSegments = segmentIntensityRing.headMap(toSegment);
+            if (!priorToSegments.isEmpty()) {
+                var priorToSegmentIntensity = segmentIntensityRing.get(priorToSegments.lastKey());
+                //logger.log(Level.INFO, String.format("ToSegment is %d PriorToSegment is value %d, intensity %d", toSegment, priorToSegments.lastKey(), priorToSegmentIntensity));
+                if (priorToSegmentIntensity == 0) {
+                    segmentIntensityRing.remove(toSegment);
+                }
+            }
+        }
+        return toSegment(segmentIntensityRing.entrySet());
+    }
+
     public List<Segment> add(int fromSegment, int toSegment, int intensity) {
+        BinaryOperator<Integer> add = (x, y) -> x + y;
+        return segmentBinaryOperation(fromSegment, toSegment, intensity, add);
+    }
+
+    public List<Segment> set(int fromSegment, int toSegment, int intensity) {
+        BinaryOperator<Integer> set = (x, y) -> y;
+        return segmentBinaryOperation(fromSegment, toSegment, intensity, set);
+    }
+
+
+    private List<Segment> toSegment(Set<Map.Entry<Integer, Integer>> entrySet) {
+        return entrySet
+                .stream()
+                .map(es -> (new Segment(es.getKey(), es.getValue()))).toList();
+    }
+
+    public List<Segment> addL(int fromSegment, int toSegment, int intensity) {
         if (segmentIntensityRing.isEmpty()) {
             segmentIntensityRing.put(fromSegment, intensity);
             segmentIntensityRing.put(toSegment, 0);
@@ -61,49 +129,5 @@ public class IntensityProcessor {
         return toSegment(segmentIntensityRing.entrySet());
     }
 
-
-    private List<Segment> toSegment(Set<Map.Entry<Integer, Integer>> entrySet) {
-        return entrySet
-                .stream()
-                .map(es -> (new Segment(es.getKey(), es.getValue()))).toList();
-    }
-
-
-//    public List[Segment] addIntensity(int fromSegment, int toSegment, int intensity) {
-//        if (segmentIntensityRing.isEmpty()) {
-//            segmentIntensityRing.put(fromSegment, 1);
-//            segmentIntensityRing.put(toSegment, 0);
-//        }
-//
-//
-//        var existingSegment = segmentIntensityRing.get(fromSegment);
-//
-//        String ale = "adleix";
-//        if (existingSegment == null) {
-//
-//        } else {
-//            var segmentTail = segmentIntensityRing.tailMap(fromSegment);
-//            if (segmentTail != null) {
-//                for (Integer key : segmentTail.keySet()) {
-//
-//
-//                }
-//
-//
-//                //String name = "d";
-//                // var cat = "cad";
-//                //String other = "da";
-//
-//
-//            } else {
-//
-//            }
-//
-//            return null;
-//
-//        }
-//
-//
-//    }
 
 }
