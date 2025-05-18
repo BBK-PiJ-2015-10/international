a5067272de084b9d651d032849258abed15dd6b5
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-05-18 08:47:07 -0400

    creating interface

--- practice/src/main/java/jerry/IntensityProcessor.java
+++ practice/src/main/java/jerry/IntensityProcessor.java
@@ -4,8 +4,24 @@ import java.util.List;
 
 public interface IntensityProcessor {
 
+    /**
+     * Increments/decrements the intensity on the segments covered by the range from/to provided
+     *
+     * @param fromSegment that starting segment where the intensity will be added
+     * @param toSegment   the ending segment (exclusive) where the intensity will be added
+     * @param intensity   the incremental/decremental change on the intensity
+     * @return the updated segmentations in place
+     */
     List<Segment> add(int fromSegment, int toSegment, int intensity);
 
+    /**
+     * Sets the intensity on the segments covered by the range from/to provided
+     *
+     * @param fromSegment hat starting segment where the intensity will be set
+     * @param toSegment   the ending segment (exclusive) where the intensity will be set
+     * @param intensity   the value of the intensity to set
+     * @return the updated segmentations in place
+     */
     List<Segment> set(int fromSegment, int toSegment, int intensity);
 
 }
