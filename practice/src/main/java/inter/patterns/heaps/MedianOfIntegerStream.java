09f28d44748eda7ed74d2b6e3f3a38c6bf8d0ba9
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-08-15 16:51:45 -0400

    solution of MediaOfIntegerStream

--- practice/src/main/java/inter/patterns/heaps/MedianOfIntegerStream.java
+++ practice/src/main/java/inter/patterns/heaps/MedianOfIntegerStream.java
@@ -1,6 +1,10 @@
 package inter.patterns.heaps;
 
+import java.util.logging.Logger;
+
 public class MedianOfIntegerStream {
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
 
     public int count = 0;
 
@@ -33,9 +37,11 @@ public class MedianOfIntegerStream {
         } else if (median.right == null) {
             median.right = new Node(valueToAdd);
         } else if (valueToAdd > median.left.value && valueToAdd < median.right.value) {
+            // if in between replaced the right
             var newNode = new Node(valueToAdd);
             newNode.next = median.right;
             median.right.prior = newNode;
+            median.right = newNode;
         } else {
             if (!(valueToAdd > median.left.value)) {
                 addLeft(valueToAdd, median.left);
@@ -54,14 +60,18 @@ public class MedianOfIntegerStream {
             if (node.next != null) {
                 addLeft(valueToAdd, node.next);
             } else {
-                node.next = new Node(valueToAdd);
+                var newNode = new Node(valueToAdd);
+                node.next = newNode;
+                newNode.prior = node;
             }
         }
         // replace
         else {
             var newNode = new Node(valueToAdd);
-            node.prior.next = newNode;
+            newNode.prior = node.prior;
             newNode.next = node;
+            node.prior.next = newNode;
+            node.prior = newNode;
         }
     }
 
@@ -71,14 +81,18 @@ public class MedianOfIntegerStream {
             if (node.next != null) {
                 addRight(valueToAdd, node.next);
             } else {
-                node.next = new Node(valueToAdd);
+                var newNode = new Node(valueToAdd);
+                node.next = newNode;
+                newNode.prior = node;
             }
         }
         // replace
         else {
             var newNode = new Node(valueToAdd);
-            node.prior.next = newNode;
+            newNode.prior = node.prior;
             newNode.next = node;
+            node.prior.next = newNode;
+            node.prior = newNode;
         }
     }
 
@@ -87,7 +101,11 @@ public class MedianOfIntegerStream {
             return 0;
         }
         if (count % 2 == 0) {
-            double medianResult = (median.left.value + median.right.value) / 2;
+            double left = median.left.value;
+            double right = median.right.value;
+            // double medianResult = (median.left.value + median.right.value) / 2;
+            double medianResult = (left + right) / 2;
+            logger.info(String.format("Left is %s, right is %s, median is %s", left, right, medianResult));
             return medianResult;
         } else {
             return median.left.value;
